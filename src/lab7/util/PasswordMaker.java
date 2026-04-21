b54b5ef9b9f5ccc4519af7f8f24ae9cde087c687
- <->
2026-04-21 09:02:40 +0300

    lab7 pr2

--- src/lab7/util/PasswordMaker.java
+++ src/lab7/util/PasswordMaker.java
@@ -1,14 +1,19 @@
 package lab7.util;
 
+
 public class PasswordMaker {
     private static final int MAGIC_NUMBER = 3;
     private static final String MAGIC_STRING;
-    private static PasswordMaker parola=null;
+
+    private static PasswordMaker instance;
+    private static int callCount = 0;
+
     private String name;
 
     static {
         StringRandomizer srand = new StringRandomizer();
         MAGIC_STRING = srand.randomString(20);
+        instance = new PasswordMaker("SingletonUser");
     }
 
     private PasswordMaker(String name) {
@@ -16,23 +21,19 @@ public class PasswordMaker {
     }
 
     public static PasswordMaker getInstance() {
-        if(parola==null){
-            parola=new PasswordMaker("Andrei");
-
-        }
-        return parola;
+        callCount++;
+        return instance;
     }
 
-    public static String getCallingCounts() {
-
+    public static int getCallingCounts() {
+        return callCount;
     }
 
     public String getPassword() {
         java.util.Random r = new java.util.Random();
-        String ln = ""+name.length();
+        String ln = "" + name.length();
         ln += r.nextInt(101);
         StringRandomizer srand = new StringRandomizer();
         return srand.randomString(MAGIC_NUMBER) + srand.randomString(10, MAGIC_STRING) + ln;
     }
 }
-
